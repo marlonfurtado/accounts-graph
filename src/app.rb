55c4cc67aa852943ce8d4571a35c34054c64daaf
marlonfurtado <marlonfurtado.rs@gmail.com>
2017-10-08 12:22:34 -0300

    Add app with graphviz

--- src/app.rb
+++ src/app.rb
@@ -1,21 +1,39 @@
 require 'graphviz'
+require_relative 'reader'
+
+file = ARGV[0]
+reader = Reader.new file
+data = reader.data
+
+data.shift  # Remove first
+data.pop  # Remove last
+
+accounts = data.map {|x| x[/\d+/]}  # All account numbers
 
 # Create a new graph
-g = GraphViz.new( :G, :type => :digraph )
+graph = GraphViz.new( :G, :type => :graph )
 
-# Create two nodes
-n1 = g.add_nodes( "01" )
-n2 = g.add_nodes( "02" )
-n3 = g.add_nodes( "03" )
-n4 = g.add_nodes( "04" )
-n5 = g.add_nodes( "05" )
+accounts.each do |node|
+    # Create nodes
+    graph.add_nodes(node.to_s)
+end
 
-# Create an edge between the two nodes
-g.add_edges( n1, n2 )
-g.add_edges( n2, n3 )
-g.add_edges( n2, n4 )
-g.add_edges( n1, n5 )
-g.add_edges( n5, n4 )
+for i in 0...accounts.size
+    nodei = data[i].split(" ")[0]
+    name_i1 = data[i].split(" ")[1]
+    name_i2 = data[i].split(" ")[2]
+
+    for j in i+1...accounts.size
+        nodej = data[j].split(" ")[0]
+        name_j1 = data[j].split(" ")[1]
+        name_j2 = data[j].split(" ")[2]
+        
+        if(name_i1 == name_j1 || name_i1 == name_j2 || name_i2 == name_j1 || name_i2 == name_j2)
+            puts "Adding edges to nodes:  #{nodei} -- #{nodej}"
+            graph.add_edges( nodei.to_s, nodej.to_s )
+        end
+    end
+end
 
 # Generate output image
-g.output( :png => File.join(File.dirname(__FILE__), '../img/example.png'))
+graph.output( :png => File.join(File.dirname(__FILE__), "../img/#{file}.png"))
